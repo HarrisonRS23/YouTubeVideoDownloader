08f9418cd08c78d5603b224edb200c690738db60
Harrison Sheldon <your_email_address@example.com>
2023-12-19 10:51:31 -0800

    add resolution box with default value and selectors

--- env/app.py
+++ env/app.py
@@ -3,6 +3,11 @@ from tkinter import ttk
 from pytube import YouTube  # library to access youtube videos
 import os  # place files in system
 
+
+def download_video():
+    print("Downloading...")
+
+
 # create root window
 root = ctk.CTk()
 
@@ -14,12 +19,12 @@ root.title("Youtube Downloader")
 
 # restrict minimize and maximize size
 root.geometry("720x480")
-root.minsize(width=720, height= 480)
+root.minsize(width=720, height=480)
 root.maxsize(width=1080, height=720)
 
 # create frame to hold content
 content_frame = ctk.CTkFrame(root)  # mount frame onto root
-content_frame.pack(fill="both", expand=True, padx=10,pady=10)  # fill the entire root
+content_frame.pack(fill="both", expand=True, padx=10, pady=10)  # fill the entire root
 
 # create a label and the entry widget for the video url
 urlLabel = ctk.CTkLabel(content_frame, text="Enter the Youtube Url Here: ")
@@ -28,8 +33,16 @@ urlLabel.pack(pady=("10p", "5p"))
 userUrl.pack(pady=("10p", "5p"))
 
 # create download button
-downloadButton = ctk.CTkButton(content_frame, text="Download")
+downloadButton = ctk.CTkButton(content_frame, text="Download", command=download_video)
 downloadButton.pack(pady=("10p", "5p"))
+
+# create a resolution selector box
+resolutionList = ["1080px", "720px", "360px", "240px"]
+resolutionVar = ctk.StringVar()
+# assign selected value to resolution variable
+resolutionComboBox = ttk.Combobox(content_frame, values=resolutionList, textvariable=resolutionVar)
+resolutionComboBox.pack(pady=("10p", "5p"))
+resolutionComboBox.set("720px")
 
 # to start the app
 root.mainloop()
